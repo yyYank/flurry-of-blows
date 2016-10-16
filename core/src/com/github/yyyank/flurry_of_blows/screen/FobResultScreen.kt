4b292881cd98142e039236d61ff8b9d4d989e711
yy_yank <yy.yank.dev.blog@gmail.com>
2016-10-16 18:58:37 +0900

    add impl Adapter

--- core/src/com/github/yyyank/flurry_of_blows/screen/FobResultScreen.kt
+++ core/src/com/github/yyyank/flurry_of_blows/screen/FobResultScreen.kt
@@ -1,9 +1,30 @@
 package com.github.yyyank.flurry_of_blows.screen
 
+import com.badlogic.gdx.ScreenAdapter
+
 /**
  * ゲームの結果画面
  * Created by yy_yank on 2016/10/16.
  */
-class FobResultScreen{
+class FobResultScreen : ScreenAdapter() {
+    override fun render(delta: Float) {
+    }
 
+    override fun resize(width: Int, height: Int) {
+    }
+
+    override fun show() {
+    }
+
+    override fun hide() {
+    }
+
+    override fun pause() {
+    }
+
+    override fun resume() {
+    }
+
+    override fun dispose() {
+    }
 }
--- core/src/com/github/yyyank/flurry_of_blows/screen/TitleScreen.kt
+++ core/src/com/github/yyyank/flurry_of_blows/screen/TitleScreen.kt
@@ -1,9 +1,30 @@
 package com.github.yyyank.flurry_of_blows.screen
 
+import com.badlogic.gdx.ScreenAdapter
+
 /**
  * タイトル画面
  * Created by yy_yank on 2016/10/16.
  */
-class TitleScreen{
+class TitleScreen : ScreenAdapter() {
+    override fun render(delta: Float) {
+    }
 
+    override fun resize(width: Int, height: Int) {
+    }
+
+    override fun show() {
+    }
+
+    override fun hide() {
+    }
+
+    override fun pause() {
+    }
+
+    override fun resume() {
+    }
+
+    override fun dispose() {
+    }
 }
