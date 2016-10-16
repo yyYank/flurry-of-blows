7299717e09997fa12f2e8b20b9207a5fab9154e5
yy_yank <yy.yank.dev.blog@gmail.com>
2016-10-16 18:53:20 +0900

    empty screen added

---
+++ core/src/com/github/yyyank/flurry_of_blows/screen/FlurryOfBlowsScreen.kt
@@ -0,0 +1,9 @@
+package com.github.yyyank.flurry_of_blows.screen
+
+/**
+ * 連打画面
+ * Created by yy_yank on 2016/10/16.
+ */
+class FlurryOfBlowsScreen {
+
+}
---
+++ core/src/com/github/yyyank/flurry_of_blows/screen/ProcessingFobScreen.kt
@@ -0,0 +1,9 @@
+package com.github.yyyank.flurry_of_blows.screen
+
+/**
+ * 連打後のプロセス画面
+ * Created by yy_yank on 2016/10/16.
+ */
+class ProcessingFobScreen{
+
+}
