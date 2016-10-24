224985b289181252c8d83904e1ed29065f6cd913
yy_yank <yy.yank.dev.blog@gmail.com>
2016-10-24 12:21:20 +0900

    define class

--- core/src/com/github/yyyank/flurry_of_blows/actor/PowerGauge.kt
+++ core/src/com/github/yyyank/flurry_of_blows/actor/PowerGauge.kt
@@ -1,5 +1,19 @@
 package com.github.yyyank.flurry_of_blows.actor
 
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.github.yyyank.flurry_of_blows.callback.Starter
+import java.util.concurrent.atomic.AtomicInteger
+
+
 /**
  * Created by yy_yank on 2016/10/21.
  */
+class PowerGauge (val skin : Skin) : Starter {
+
+    override var callback: Runnable? = null
+
+    private val power = AtomicInteger(0)
+
+    override fun start() {
+    }
+}
